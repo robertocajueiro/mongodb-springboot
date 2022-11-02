13d971eacdf7172ad1d19c5a82b6c8d14bf4156e
robertocajueiro <beto.cajueiro@gmail.com>
2022-11-02 14:32:42 -0300

    first commit

---
+++ src/main/java/com/javatech/repository/TaskRepository.java
@@ -0,0 +1,14 @@
+package com.javatech.repository;
+
+import com.javatech.model.Task;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
+public interface TaskRepository extends MongoRepository<Task,String> {
+    List<Task> findBySeverity(int severity);
+
+    @Query("{assignee: ?0}")
+    List<Task> getTasksByAssignee(String assignee);
+}
